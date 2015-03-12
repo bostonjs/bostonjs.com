6e006bde632242ffd6bdd0163230fec8321ca858
adam j. sontag <ajpiano@ajpiano.com>
2015-03-12 12:29:45 -0400

    Update CFP form and get it to post submissions to Submittable

--- app/index.js
+++ app/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const jade = require('jade');
 const _ = require('lodash');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const BODY_LIMIT = '500kb';
 
 var app = express();
+var transporter = nodemailer.createTransport();
 app.locals.moment = require('moment');
 var meetups = _.map(require('./data/meetups').results, function(m, i) {
   m.index = i+1;
   return m;
 });
 
+app.use(bodyParser.json({ limit: BODY_LIMIT }));
+app.use(bodyParser.urlencoded({ limit: BODY_LIMIT, extended: true }));
+
 app.use(express.static(__dirname + '/public'));
+
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
@@ -34,13 +42,37 @@ app.get('/events/', function(req, res){
 });
 
 app.get('/submit-a-talk/', function(req, res){
-  console.log(req);
-  res.render('submit', {submission: "foo"});
+  res.render('submit');
 });
 
 app.post('/submit-a-talk/', function(req, res){
-  console.log(req);
-  res.render('submit', {submission: "foo"});
+  var form = req.body;
+  var destination = 'bocoup+38004@submissions.submittable.com'; 
+  var submissionContents = [
+    '#name:' + form.name,
+    '#email:' + form.email,
+    '\n',
+    '<strong>Description</strong>',
+    form.description || '',
+    '\n',
+    '/--JSON--/',
+    JSON.stringify(form),
+    '/----/'
+  ];
+  transporter.sendMail({
+    from: 'submittable-relayer@bocoup.com', // Not a real e-mail address (yet)
+    to: destination,
+    subject: form.title,
+    text: submissionContents.join('\n')
+  }, function(error, info) {
+    if ( error ) {
+      console.log( error );
+      res.status(400).json({error: error});
+    }
+    res.render('submit', {submission: req.body});
+    //res.json({success: 'Your submission, "' + form.title +'", has been received. You should receive a confirmation in your e-mail shortly!'});
+
+  });
 });
 
 module.exports = app;
